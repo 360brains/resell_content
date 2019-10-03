12a36fd35a4f1c70b5ca7718a48a03d4ca40b0b7
Muhammad Bilal <bilal.bs143@gmail.com>
2019-10-03 16:33:52 +0500

    changes

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -10,6 +10,10 @@
             </li>
             <li>
                 <span>Categories</span>
+                <i class="fa fa-circle"></i>
+            </li>
+            <li>
+                <span>Create</span>
             </li>
         </ul>
         <div class="page-toolbar">
@@ -22,19 +26,45 @@
 
     </div>
     <h3 class="page-title">Categories
+        <small>Create Category</small>
     </h3>
 
     <div class="row">
         <div class="col-md-12">
             <!-- BEGIN VALIDATION STATES-->
-            <div class="portlet light portlet-fit portlet-form bordered">
+            <div class="portlet light portlet-fit bordered">
 
-                <div class="portlet-body">
-                    <!-- BEGIN FORM-->
+                <div class="portlet-body flip-scroll">
+                    <table class="table table-bordered table-striped flip-content">
+                        <thead class="flip-content">
+                        <tr>
+                            <th width="20%"> Code </th>
+                            <th> Company </th>
+                            <th class="numeric"> Price </th>
+                            <th class="numeric"> Change </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td> AGK </td>
+                            <td> AGL ENERGY LIMITED </td>
+                            <td class="numeric"> $13.82 </td>
+                            <td class="numeric"> +0.02 </td>
+                        </tr>
+                        <tr>
+                            <td> AGO </td>
+                            <td> ATLAS IRON LIMITED </td>
+                            <td class="numeric"> $3.17 </td>
+                            <td class="numeric"> -0.02 </td>
+                        </tr>
+                        </tbody>
+                    </table>
 
-                    <!-- END FORM-->
                 </div>
+
+
             </div>
         </div>
     </div>
 @endsection
+
