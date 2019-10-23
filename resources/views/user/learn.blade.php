1dae46bfcc7b505780768549c76a508fc4ffdace
Basit Ali <bilal.bs143@gmail.com>
2019-10-23 18:19:30 +0500

    jhg

--- resources/views/user/learn.blade.php
+++ resources/views/user/learn.blade.php
@@ -26,26 +26,25 @@
 
                                         <!-- Jon Youshaei -->
 
-                                        <span class="name">Jon Youshaei</span>
                                         <p class="instructor_title">
-                                            {{ $training->descriotion }}
+                                            {!! $training->description !!}
                                         </p>
 
                                         <div class="bottom_fix_cource">
                                             <div class="left_con_cource float-left">
-                                            <span><sup>$</sup><strong>45</strong></span>
+                                                @if($training->is_paid )
+                                                    <span><sup>$</sup><strong>{{ $training->fee }}</strong></span>
+                                                @else
+                                                    <span><strong>FREE</strong></span>
+                                                @endif
                                             </div>
 
                                             <div class="right_con_cource float-right">
 
-                                            <i class="fas fa-video" title="Video Lecture"></i>
+                                            <i class="{{ $training->method == 'Video' ? 'fas fa-video' : 'fas fa-chalkboard-teacher'}}" title="Video Lecture"></i>
 
                                             <div class="right_con_cource">
-                                                @if($training->is_paid )
-                                                <span><sup>$</sup><strong>{{ $training->fee }}</strong></span>
-                                                @else
-                                                <span><strong>FREE</strong></span>
-                                                @endif
+
 
                                             </div>
                                         </div>
@@ -61,51 +60,6 @@
                     @endforelse
 
 
-{{--                    <div class="col-md-4">--}}
-{{--                        <div class="spacing">--}}
-{{--                            <a href="/courses/branding-strategy-and-design-for-small-businesses">--}}
-{{--                                <div class="border_cource  blog-img-course">--}}
-{{--                                    <div class="blog-img">--}}
-{{--                                        <img src="https://thinkific-import.s3.amazonaws.com/114242/gkpr2xmcSGe7c4p0tfsQ_New%20course%20Thumbnail%20780x440-min.jpg"--}}
-{{--                                             alt="Brand Strategy and Design for Small Businesses">--}}
-{{--                                        <div class="blog_overlay_new"--}}
-{{--                                             data-url="/courses/branding-strategy-and-design-for-small-businesses">--}}
-{{--                                            <p>learn more<span class="green_dot">.</span></p>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-
-{{--                                    <div class="cource_pading">--}}
-{{--                                        <h3 class="product_name">Brand Strategy and Design for Small Businesses</h3>--}}
-
-{{--                                        <div class="ratting_cource">--}}
-
-{{--                                            <i class="fa fa-star"></i><strong>4.9 </strong> (44)--}}
-
-{{--                                        </div>--}}
-
-{{--                                        <!-- Haylee Powers -->--}}
-
-{{--                                        <span class="name">Haylee Powers</span>--}}
-{{--                                        <p class="instructor_title">--}}
-{{--                                            Haylee is an Emmy award-winning designer specializing in brand strategy--}}
-{{--                                            and--}}
-{{--                                            design for compelling businesses.--}}
-{{--                                        </p>--}}
-
-{{--                                        <div class="bottom_fix_cource">--}}
-{{--                                            <div class="left_con_cource float-left">--}}
-{{--                                            <span><sup>$</sup><strong>40</strong></span>--}}
-{{--                                            </div>--}}
-
-{{--                                            <div class="right_con_cource float-right">--}}
-{{--                                            <i class="fas fa-chalkboard-teacher" title="Online Session"></i>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
                 </div>
             </div>
         </div><!-- .container -->
