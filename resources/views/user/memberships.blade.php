2cd8f0660ff8945cce9e1080ec500abf5bd6feb7
Basit Ali <bilal.bs143@gmail.com>
2019-11-05 10:54:01 +0500

    abc

---
+++ resources/views/user/memberships.blade.php
@@ -0,0 +1,80 @@
+@extends('user.layouts.master')
+
+@section('content')
+
+    <div class="page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="token-statistics card card-token height-auto">
+                        <div class="card-innr">
+                            <div class="token-balance token-balance-with-icon">
+                                <div class="token-balance-icon"><img src="http://127.0.0.1:8000/user/images/logo-light-sm.png" alt="logo"></div>
+                                <div class="token-balance-text">
+                                    <h6 class="card-sub-title">Total Earned</h6><span class="lead"> 0 <span>Rs</span></span>
+                                </div>
+                            </div>
+                            <div class="token-balance token-balance-s2">
+                                <h6 class="card-sub-title">Your Stats</h6>
+                                <ul class="token-balance-list">
+                                    <li class="token-balance-sub"><span class="lead">0</span><span class="sub">Tasks</span></li>
+                                    <li class="token-balance-sub"><span class="lead">
+                                                                                                                                0
+                                    </span><span class="sub">Trainings</span></li>
+                                    <li class="token-balance-sub"><span class="lead">0</span><span class="sub">Transactions</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="token-statistics card card-token height-auto">
+                        <div class="card-innr">
+                            <div class="token-balance token-balance-with-icon">
+                                <div class="token-balance-icon"><img src="http://127.0.0.1:8000/user/images/logo-light-sm.png" alt="logo"></div>
+                                <div class="token-balance-text">
+                                    <h6 class="card-sub-title">Total Earned</h6><span class="lead"> 0 <span>Rs</span></span>
+                                </div>
+                            </div>
+                            <div class="token-balance token-balance-s2">
+                                <h6 class="card-sub-title">Your Stats</h6>
+                                <ul class="token-balance-list">
+                                    <li class="token-balance-sub"><span class="lead">0</span><span class="sub">Tasks</span></li>
+                                    <li class="token-balance-sub"><span class="lead">
+                                                                                                                                0
+                                    </span><span class="sub">Trainings</span></li>
+                                    <li class="token-balance-sub"><span class="lead">0</span><span class="sub">Transactions</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="token-statistics card card-token height-auto">
+                        <div class="card-innr">
+                            <div class="token-balance token-balance-with-icon">
+                                <div class="token-balance-icon"><img src="http://127.0.0.1:8000/user/images/logo-light-sm.png" alt="logo"></div>
+                                <div class="token-balance-text">
+                                    <h6 class="card-sub-title">Total Earned</h6><span class="lead"> 0 <span>Rs</span></span>
+                                </div>
+                            </div>
+                            <div class="token-balance token-balance-s2">
+                                <h6 class="card-sub-title">Your Stats</h6>
+                                <ul class="token-balance-list">
+                                    <li class="token-balance-sub"><span class="lead">0</span><span class="sub">Tasks</span></li>
+                                    <li class="token-balance-sub"><span class="lead">
+                                                                                                                                0
+                                    </span><span class="sub">Trainings</span></li>
+                                    <li class="token-balance-sub"><span class="lead">0</span><span class="sub">Transactions</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- .container -->
+    </div>
+
+@endsection
+
