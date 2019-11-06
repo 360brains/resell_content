d567f0beca7838aaceb5e5ddf552b54d656e8a50
Basit Ali <bilal.bs143@gmail.com>
2019-11-06 11:16:06 +0500

    a

--- app/Http/Controllers/User/TransactionController.php
+++ app/Http/Controllers/User/TransactionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Models\Transaction;
+use App\Models\Withdraw;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -12,5 +13,9 @@ class TransactionController extends Controller
         $user_id                = auth()->user()->id;
         $data['transactions']   = Transaction::where('user_id', $user_id)->orderBy('created_at', 'desc')->paginate(10);
         return view('user.transactions', $data);
+    }
+    public function withdrawIndex(){
+        $data['withdrawRequests']   = Withdraw::with('user')->orderBy('status', 'desc')->get();
+        return view('admin.withdraws.index', $data);
     }
 }
