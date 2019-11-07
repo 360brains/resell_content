ca829fa7831d8c31594a814083ddccb225d5d4e0
Basit Ali <bilal.bs143@gmail.com>
2019-11-07 17:10:24 +0500

    Basit 7/11

---
+++ resources/views/user/payment.blade.php
@@ -0,0 +1,25 @@
+@extends('user.layouts.master')
+
+@section('content')
+
+    <div class="page-content">
+        <div class="container">
+            <div class="card content-area">
+                <div class="card-innr">
+                    <div class="card-head">
+                        <h4 class="card-title">Payment</h4></div>
+                    <a href="" class="btn btn-danger">Generate Bank Voucher</a>
+                    <a href="" class="btn btn-danger">Pay Online</a>
+                    <div>
+                    </div>
+                </div>
+                <!-- .card-innr -->
+            </div>
+            <!-- .card -->
+        </div>
+        <!-- .container -->
+    </div>
+
+
+
+@endsection
