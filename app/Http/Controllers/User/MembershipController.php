36e557c6156695443084d01b6dbc54a6b90fa8f3
Basit Ali <bilal.bs143@gmail.com>
2019-11-14 18:32:16 +0500

    basit 11/14

--- app/Http/Controllers/User/MembershipController.php
+++ app/Http/Controllers/User/MembershipController.php
@@ -12,4 +12,22 @@ class MembershipController extends Controller
         $data['membership'] = Membership::where('name', 'Premium')->first();
         return view('user.memberships', $data);
     }
+
+    public function buy(Request $request, $id){
+        $membership = Membership::where('id', $id)->first();
+        if ($request->check == 'on'){
+            if (auth()->user()->balance >= $membership->price){
+                $date = new \DateTime("+2 months");
+                $data = [
+                    'user_id' => auth()->user()->id,
+                    'membership_id' => $membership->id,
+                    'status' => 'Bought',
+                    'deadline' => $date,
+                ];
+                $responce = Membership::create($data);
+            }
+        }else{
+            return redirect()->back()->with("error", "Please agree to the membership purchase agreement.");
+        }
+    }
 }
