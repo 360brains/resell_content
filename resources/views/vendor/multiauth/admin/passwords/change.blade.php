000a77a0879619f2640228639491e4579819dd53
rehabzafar <rehabzafar9@gmail.com>
2020-04-03 20:46:54 +0500

    admin updates auth

--- resources/views/vendor/multiauth/admin/passwords/change.blade.php
+++ resources/views/vendor/multiauth/admin/passwords/change.blade.php
@@ -1,57 +1,47 @@
-@extends('multiauth::layouts.app')
+@extends('admin.layouts.master')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ ucfirst(config('multiauth.prefix')) }} Change Your Password</div>
-
-                <div class="card-body">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="portlet light bordered">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class="icon-key font-dark"></i>
+                        <span class="caption-subject font-dark sbold uppercase">{{ ucfirst(config('multiauth.prefix')) }} Change Your Password</span>
+                    </div>
+                </div>
+                <div class="portlet-body form">
                     <form method="POST" action="{{ route('admin.password.change') }}" aria-label="{{ __('Admin Change Password') }}">
                         @csrf
-                        <div class="form-group row">
-                            <label for="oldPassword" class="col-md-4 col-form-label text-md-right">{{ __('Old Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="oldPassword" type="password" class="form-control{{ $errors->has('oldPassword') ? ' is-invalid' : '' }}" name="oldPassword" value="{{ $oldPassword ?? old('oldPassword') }}"
+                        <div class="form-body">
+                        <div class="form-group">
+                            <label >{{ __('Old Password') }}</label>
+                                <input id="oldPassword" type="password" class="form-control spinner{{ $errors->has('oldPassword') ? ' is-invalid' : '' }}" name="oldPassword" value="{{ $oldPassword ?? old('oldPassword') }}"
                                     required autofocus> @if ($errors->has('oldPassword'))
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('oldPassword') }}</strong>
                                     </span> @endif
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+                        <div class="form-group">
+                            <label>{{ __('Password') }}</label>
+                                <input id="password" type="password" class="form-control spinner{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
                                     required> @if ($errors->has('password'))
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span> @endif
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                        <div class="form-group">
+                            <label>{{ __('Confirm Password') }}</label>
+                                <input id="password-confirm" type="password" class="form-control spinner" name="password_confirmation" required>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="form-actions text-center">
+                                <button type="submit" class="btn green">
                                     {{ __('Change Password') }}
                                 </button>
-                            </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
