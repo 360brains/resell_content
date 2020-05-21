6ad3f979b5ee507e36152b4d3692ea5ced3f5a00
rehabzafar <rehabzafar9@gmail.com>
2020-05-21 22:23:15 +0500

    add Google Analytics

--- resources/views/user/n-includes/head.blade.php
+++ resources/views/user/n-includes/head.blade.php
@@ -2,6 +2,16 @@
     <meta charset="utf-8">
     <meta name="author" content="Softnio">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112237949-8"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-112237949-8');
+    </script>
+
     <!-- Fav Icon -->
     <link rel="shortcut icon" href="{{ asset('user/images/favicon.png') }}">
     <!-- Site Title  -->
